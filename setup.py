054256fb4679280b21330638b5f461e93ca3f633
Mirko Mälicke <mirko@maelicke-online.de>
2020-08-14 09:26:00 +0200

    added setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+def version():
+    with open('VERSION') as f:
+        return f.read().strip()
 
 
-setup('hydenv')
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def requirements():
+    with open('requirements.txt') as f:
+        return f.read().split('\n')
+
+
+setup(
+    name='hydenv',
+    version=version(),
+    author='Mirko Mälicke',
+    author_email='mirko@hydrocode.de',
+    description='Database management tools for data lecture',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements(),
+    packages=find_packages(),
+    zip_safe=False
+)
