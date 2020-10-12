f2652d0c3f396bbb9cbdd18d295782a347936d22
Mirko Mälicke <mirko@maelicke-online.de>
2020-10-12 09:48:23 +0200

    Models updates

--- hydenv/models.py
+++ hydenv/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy import Integer, String, DateTime, Numeric
 from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
@@ -13,8 +13,13 @@ class Metadata(Base):
     # columns
     id = Column(Integer, primary_key=True)
     device_id = Column(Integer, nullable=False)
+    sensor_id = Column(Integer, ForeignKey('sensors.id'))
+    semester_id = Column(Integer, ForeignKey('semester_id'))
     location = Column(Geometry('POINT', srid=4326), nullable=False)
     description = Column(String, nullable=False)
+
+    # relationships
+    semester = relationship('Semester', back_populates='metadata')
 
 
 class Variable(Base):
@@ -24,6 +29,30 @@ class Variable(Base):
     name = Column(String(64), nullable=False, unique=True)
     unit = Column(String(30), nullable=False)
     comment = Column(String)
+
+
+class Sensor(Base):
+    __tablename__ = 'sensors'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(), nullable=False)
+
+
+class Semester(Base):
+    __tablename__ = 'semester'
+
+    # columns
+    id = Column(Integer, primary_key=True)
+    short = Column(String(8), nullable=False)
+    full_name = Column(String(), nullable=False)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+
+    # relationships
+    metadata = relationship('Metadata', back_populates='semester')
+
+    def __str__(self):
+        return self.short
 
 
 class Quality(Base):
