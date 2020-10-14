6cba5020193097f235dd94f7477456b1ce62113f
Mirko Mälicke <mirko@maelicke-online.de>
2020-10-14 15:06:37 +0200

    explain now supported by backend

--- hydenv/backend.py
+++ hydenv/backend.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from sqlalchemy import create_engine
 
 from hydenv.util.json import sqlencoder
+from hydenv.database import HydenvDatabase
 
 app = Flask(__name__)
 CORS(app)
@@ -26,31 +27,29 @@ def execute():
     # get SQL query
     safe = data.get('safe', True)
     sql = data.get('sql')
+    explain = bool(data.get('explain', False))
     if sql is None:
         return jsonify({'message': 'No SQL query given.'}), 404
     
-    # enable safe mode
-    if safe:
-        sql = sql.split(';')[0]
-        if not sql.lower().startswith('select'):
-            return jsonify({'In safe mode, only SELECT queries are allowed'}), 403
+    # build the CLI
+    cli = HydenvDatabase(connection=app.config['DB_URI'])
 
-    # run 
-    engine = create_engine(app.config['DB_URI'])
-    with engine.connect() as con:
-        t2 = dt.now()
-        try:
-            res = con.execute(sql)
-        except Exception as e:
-            return jsonify({
+    # run
+    t2 = dt.now()
+    try:
+        res = cli.execute(sql=sql, safe=safe, json=True)
+    except Exception as e:
+        return jsonify({
                 'message': 'Run errored!',
                 'params': data,
                 'error': True,
                 'data': [dict(errorMessage=str(e))]
             }, )
-        t3 = dt.now()
 
-    return jsonify({
+    t3 = dt.now()
+    
+    # build response object
+    response = {
         'message': 'Run successful',
         'params': data,
         'data': [{k: sqlencoder(v) for k,v in r.items()} for r in res],
@@ -58,7 +57,44 @@ def execute():
             backendTime=(t3 - t2).total_seconds(), 
             executionTime=(t3 - t1).total_seconds()
         )
-    })
+    }
+
+    # run explain if needed
+    if explain:
+        try:
+            exp_text = cli.explain(sql=sql, fmt='text')
+        except Exception as e:
+            exp_text = str(e)
+        response['explain'] = exp_text
+    
+    # return
+    return jsonify(response)
+
+
+
+@app.route('/api/v1/explain', methods=['GET'])
+def explain():
+    data = request.json if request.json else {}
+    data.update(request.args)
+
+    # get SQL query and params
+    full = data.get('full', False)
+    fmt = data.get('format', data.get('fmt', 'json'))
+    sql = data.get('sql')
+    if sql is None:
+        return jsonify({'message': 'No SQL query given.'}), 404
+    
+    # run the CLI
+    cli = HydenvDatabase(connection=app.config['DB_URI'])
+    exp = cli.explain(sql=sql, fmt=fmt, full=full)
+
+    # return
+    if fmt == 'json':
+        return jsonify(exp)
+    else:
+        return exp
+
+
 
 
 def run(db_uri, debug=False, port=5000, host='localhost'):
