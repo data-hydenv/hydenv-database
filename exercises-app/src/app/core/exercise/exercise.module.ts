2d02e2015ee075fbebb5e85fde9d81ffcbf6f267
Mirko Mälicke <mirko@maelicke-online.de>
2020-11-08 16:14:52 +0100

    import order

--- exercises-app/src/app/core/exercise/exercise.module.ts
+++ exercises-app/src/app/core/exercise/exercise.module.ts
@@ -6,19 +6,20 @@ import { ClarityModule } from '@clr/angular';
 import { CodeEditorModule } from '@ngstack/code-editor';
 import { ColorPickerModule } from 'ngx-color-picker';
 
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js';
+PlotlyModule.plotlyjs = PlotlyJS;
+// import { PlotlyViaCDNModule as PlotlyModule } from 'angular-plotly.js';
+// PlotlyModule.setPlotlyVersion('latest');
+// PlotlyModule.setPlotlyBundle('basic');
+
 import { ExerciseBodyComponent } from './exercise-body/exercise-body.component';
 import { ExerciseCompareComponent } from './exercise-compare/exercise-compare.component';
 import { SqlInputComponent } from './sql-input/sql-input.component';
 import { ResultPlotComponent } from './result-plot/result-plot.component';
 import { SqlResultComponent } from './sql-result/sql-result.component';
-
-import * as PlotlyJS from 'plotly.js/dist/plotly.js';
-import { PlotlyModule } from 'angular-plotly.js';
 import { ExerciseProgressDetailComponent } from './exercise-progress-detail/exercise-progress-detail.component';
-PlotlyModule.plotlyjs = PlotlyJS;
-// import { PlotlyViaCDNModule as PlotlyModule } from 'angular-plotly.js';
-// PlotlyModule.setPlotlyVersion('latest');
-// PlotlyModule.setPlotlyBundle('basic');
+
 
 @NgModule({
   declarations: [
